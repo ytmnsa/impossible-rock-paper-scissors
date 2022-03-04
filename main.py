5d8f91a9b9b87d9f652e78a51380a95288806ddf
Moayyed <85253172+ytmnsa@users.noreply.github.com>
2022-03-04 15:24:52 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+x = input("""Rock Paper Scissors!
+A) Rock
+B) Paper
+C) Scissors :""")
+
+if x == "a":
+  print("Paper!, You lose!")
+elif x == "b":
+  print("Scissors, You lose!")
+elif x == "c":
+  print("Rock! , You lose!")
+#caps
+if x == "A":
+  print("Paper!, You lose!")
+elif x == "B":
+  print("Scissors, You lose!")
+elif x == "C":
+  print("Rock! , You lose!")
